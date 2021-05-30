045b3916e46b7cc04a395bc2097491b8baa65731
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-05-30 20:17:56 +0900

    Update Halloween-Sale.cpp

--- Halloween-Sale.cpp
+++ Halloween-Sale.cpp
@@ -1,13 +1,13 @@
 int howManyGames(int p, int d, int m, int s)
 {
-    int result = 0;
+    int count = 0;
     
     while (s >= p)
     {
         s -= p;
         p = std::max(p-d, m);
-        result++;
+        count++;
     }
     
-    return result;
+    return count;
 }
