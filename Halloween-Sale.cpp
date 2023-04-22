352b17f32ad3353585b076a1cfee1adeba4bd0dd
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-04-22 14:05:21 +0900

    Update Halloween-Sale.cpp

--- Halloween-Sale.cpp
+++ Halloween-Sale.cpp
@@ -1,3 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'howManyGames' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER p
+ *  2. INTEGER d
+ *  3. INTEGER m
+ *  4. INTEGER s
+ */
+
 int howManyGames(int p, int d, int m, int s)
 {
     int count = 0;
