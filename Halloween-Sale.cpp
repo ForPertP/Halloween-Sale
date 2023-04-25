1f6ceab475560f133a528ced38b198188904d00f
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-04-25 09:42:25 +0900

    Update Halloween-Sale.cpp

--- Halloween-Sale.cpp
+++ Halloween-Sale.cpp
@@ -17,17 +17,37 @@ vector<string> split(const string &);
  *  4. INTEGER s
  */
 
-int howManyGames(int p, int d, int m, int s)
-{
+int howManyGames(int p, int d, int m, int s) {
+    // Return the number of games you can buy
     int count = 0;
+
+    // while (s >= p) {
+    //     count++;
+    //     s -= p;
+    //     p -= d;
+    //     if (p < m) {
+    //         p = m;
+    //     }
+    // }
     
-    while (s >= p)
-    {
-        s -= p;
-        p = std::max(p-d, m);
+    // while (s >= p)
+    // {
+    //     s -= p;
+    //     p = std::max(p-d, m);
+    //     count++;
+    // }
+   
+    while (s >= p) {
         count++;
+        s -= p;
+        p -= d;
+        if (p < m) {
+            p = m;
+            count += s / p;
+            s %= p;
+        }
     }
-    
+            
     return count;
 }
 
@@ -55,4 +75,43 @@ int main()
     fout.close();
 
     return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
 }
