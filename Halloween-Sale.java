1687f1c69b1cb2ec5b04fc3a971f23b9aba84f66
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-05-01 08:58:46 +0900

    Update Halloween-Sale.java

--- Halloween-Sale.java
+++ Halloween-Sale.java
@@ -26,7 +26,21 @@ class Result {
     public static int howManyGames(int p, int d, int m, int s) {
         int count = 0;
         
-       
+        // while (s >= p) {
+        //     count++;
+        //     s -= p;
+        //     p -= d;
+        //     if (p < m) {
+        //         p = m;
+        //     }
+        // }
+        
+        // while (s >= p) {
+        //     s -= p;
+        //     p = Math.max(p-d, m);
+        //     count++;
+        // }
+        
         while (s >= p) {
             count++;
             s -= p;
